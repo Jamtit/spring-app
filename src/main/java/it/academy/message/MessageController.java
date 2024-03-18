dc5e11be3148838d5ff314f2023faa2e62221416
Jamtit <95487835+Jamtit@users.noreply.github.com>
2024-03-18 15:37:34 +0200

    Update MessageController.java

--- src/main/java/it/academy/message/MessageController.java
+++ src/main/java/it/academy/message/MessageController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@CrossOrigin({"https://angular-template.onrender.com/"})
+@CrossOrigin({"https://angular-template.onrender.com"})
 @RequestMapping("messages")
 @AllArgsConstructor
 public class MessageController {
